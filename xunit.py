647e4e8c5e2a239cce07cffd6648d705fdaa526e
Koji Shimada <koji.shimada@enishi-tech.com>
2017-03-26 13:41:35 +0900

    Add test

--- xunit.py
+++ xunit.py
@@ -18,6 +18,6 @@ class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run()
-        assert("setUp testMethod " == test.log)
+        assert("setUp testMethod tearDown " == test.log)
 
 TestCaseTest("testTemplateMethod").run()
